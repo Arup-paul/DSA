e3f85f9bbe9ca1c1c11466833b94d6829498d58e
arup-paul <aruppaulctg@gmail.com>
2024-01-03 19:05:55 +0600

    dsa

---
+++ hash tables/main.js
@@ -0,0 +1,25 @@
+let user = {
+    age:23,
+    name:'Arup Paul',
+    magic:true,
+    scream: {
+        function() {
+            console.log('ahhh!');
+        }
+    }
+}
+
+console.log(user.age) //O(1)
+
+user.spell = 'hash';
+console.log(user) //O(1)
+
+user.scream(); //O(1)
+
+
+const a = new Map()
+
+
+
+
+
